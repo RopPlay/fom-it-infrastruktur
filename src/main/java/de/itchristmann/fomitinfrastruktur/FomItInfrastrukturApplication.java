47eca6b2da2fd40239d3aa592760255a06d6cf9d
Jan-Lukas Christmann <info@it-christmann.de>
2024-02-19 19:22:32 +0100

    init

---
+++ src/main/java/de/itchristmann/fomitinfrastruktur/FomItInfrastrukturApplication.java
@@ -0,0 +1,13 @@
+package de.itchristmann.fomitinfrastruktur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FomItInfrastrukturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FomItInfrastrukturApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/itchristmann/fomitinfrastruktur/FomItInfrastrukturApplicationTests.java
@@ -0,0 +1,13 @@
+package de.itchristmann.fomitinfrastruktur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FomItInfrastrukturApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
